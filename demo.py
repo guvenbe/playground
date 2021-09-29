9452cb4e2cb3a186afc37d8e4d77c50672dd1fae
Bora Guven <guvenbe@gmail.com>
2021-09-28 22:14:12 -0400

    initial

---
+++ demo.py
@@ -0,0 +1,10 @@
+p = input('enter value for p ')
+n = input('enter value for n ')
+r = input('enter value for r ')
+
+p = int(p)
+n = int(n)
+r = int(r)
+
+si = (p * n * r) / 100
+print('si={}', si)
