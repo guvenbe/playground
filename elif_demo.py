94f8b65090d0190d12c0656afd9ff76f586af71d
Bora Guven <guvenbe@gmail.com>
2021-09-30 21:02:54 -0400

    if and elfi

---
+++ elif_demo.py
@@ -0,0 +1,9 @@
+mark = int(input('enter you mark '))
+if mark >= 90:
+    print('Grade A')
+elif mark >= 70:
+    print('Grade B')
+elif mark >= 60:
+    print('Grade C')
+elif mark >= 50:
+    print('Grade D')
